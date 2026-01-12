a86653e6b72ceabf7e163f0ba8c1081af4adc8e0
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-12 14:21:18 +0400

    Add files via upload

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,40 +1,35 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
-const isDev = process.env.NODE_ENV === 'development' || !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.cjs'),
-      contextIsolation: true,
-      nodeIntegration: false,
-      sandbox: true
-    }
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    // icon: path.join(__dirname, '../public/icon.png') // Uncomment if you have an icon
   });
 
-  if (isDev) {
-    win.loadURL('http://localhost:5173');
-  } else {
-    // في الإنتاج، يحمل الملف الذي تم بناؤه
+  // محاولة الاتصال بسيرفر التطوير، وإذا فشل (في حالة البناء النهائي) يتم تحميل الملف
+  win.loadURL('http://localhost:5173').catch(() => {
     win.loadFile(path.join(__dirname, '../dist/index.html'));
-  }
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
+
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
-
-ipcMain.on('toMain', (event, args) => {
-  console.log(args);
-  event.sender.send('fromMain', 'تم الاستلام');
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
