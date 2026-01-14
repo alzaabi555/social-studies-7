c905ad3eeef2b6d7348521d23371e20f2ee8374f
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-15 03:10:21 +0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
     plugins: [react()],
-    // IMPORTANT: relative base is required for Capacitor and file:// protocol to work offline
     base: './', 
+    resolve: {
+      alias: {
+        '@': path.resolve('./'),
+      },
+    },
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
